5be988d3a299b3278b9150e56d03941b1533cccc
AnthonyScott <anthony.scott1005@gmail.com>
2023-04-11 14:49:46 -0400

    Hide env variables

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-04_%m*9yv&40@(jet#**u##&buwe4pr52hma75h_8rb3dxa2*@'
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
