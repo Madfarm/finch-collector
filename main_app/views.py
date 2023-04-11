8a61a7f2d4fc0c171a0d30659ea9dd5d8729f6af
AnthonyScott <anthony.scott1005@gmail.com>
2023-04-11 15:29:52 -0400

    Add index page functionality, finch data, and styling

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+finches =  [
+    {'species': 'Common Chaffinch', 'mass': 0.73, 'sexually_dimorphic': True},
+    {'species': 'House Finch', 'mass': 0.72, 'sexually_dimorphic': True},
+]
+
 # Create your views here.
 def finches_index(request):
-    return render(request, 'finches/index.html')
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
 
 def home(request):
     return render(request, 'home.html')
