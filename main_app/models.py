efa9c0d318e10a86eb4c760d4cdbaf06629ad0f2
AnthonyScott <anthony.scott1005@gmail.com>
2023-04-12 13:34:13 -0400

    Add model for finches and create 2 data entities in postgres database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    species = models.CharField(max_length=100)
+    mass = models.FloatField()
+    sexual_dimorphism = models.BooleanField()
+
+    def __str__(self):
+       return f"{self.species} mass:{self.mass} sexually dimorphic:{self.sexual_dimorphism}"
