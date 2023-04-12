ef54f675c580f1492649fecc8aaec3c6887f8209
AnthonyScott <anthony.scott1005@gmail.com>
2023-04-12 13:49:14 -0400

    Add index functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('finches/', views.finches_index, name="index"),
     path('about/', views.about, name="about"),
+    path('finches/<int:finch_id>', views.finches_details, name='details')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Finch
 
 finches =  [
     {'species': 'Common Chaffinch', 'mass': 0.73, 'sexually_dimorphic': True},
@@ -7,6 +8,7 @@ finches =  [
 
 # Create your views here.
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
     })
@@ -16,3 +18,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def finches_details(request, finch_id):
+    pass
